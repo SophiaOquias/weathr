fbd04bf20b09cd4cb6b198ca781f771805f32964
Sophia <sophia.oquias@gmail.com>
2024-09-10 10:54:31 +0800

    added display image functionality. don't even know if i'm really going to use this rawr

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -1,6 +1,11 @@
 use reqwest;
 use serde::Deserialize;
 
+use image::{DynamicImage, ImageFormat};
+use termimage::{ops, util, AnsiOutputFormat, Options}; 
+use std::{io::stdout, path::PathBuf}; 
+use term_size; 
+
 #[derive(Deserialize)]
 pub struct WeatherResponse {
     pub location: Location, 
@@ -40,4 +45,27 @@ pub fn get_weather(query: String) -> Result<WeatherResponse, Box<dyn std::error:
         .json()?;
 
     Ok(resp) 
+}
+
+fn display_image(path: String) {
+    let (_w, _h) = term_size::dimensions().unwrap(); 
+    let (w, h) = (_w as u32, _h as u32); 
+
+    let opts = Options {
+        image: (String::from(path.clone()), PathBuf::from(path)),
+        size: (w, h),
+        preserve_aspect: true,
+        ansi_out: Some(AnsiOutputFormat::Truecolor),
+    };
+
+    let format = ops::guess_format(&opts.image)
+        .expect("Unable to extract format");
+    let img = ops::load_image(&opts.image, format)
+        .expect("Unable to load image");
+    let dimensions = (img.width(), img.height()); 
+    println!("image dimensions: {:?}", dimensions); 
+    let img_s = ops::image_resized_size(dimensions, opts.size, opts.preserve_aspect);
+    let resized = ops::resize_image(&img, img_s); 
+
+    ops::write_ansi_truecolor(&mut stdout(), &resized);
 }
--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,16 @@
 
 mod api; 
 // TODO: edit interface 
+// load image in terminal (icon for weather)
+
+
 // TODO: get input via cli using clap? 
 fn main() {
     let query = "cebu".to_string(); 
     match api::get_weather(query) {
         Ok(weather) => {
-            println!("Temperature in {} is {} C", weather.location.name, weather.current.temp_c); 
-            println!("It is {}", weather.current.condition.text); 
+            println!("Temperature in {} is currently {} C⁰", weather.location.name, weather.current.temp_c); 
+            println!("It is going to be {}", weather.current.condition.text); 
             println!("Img url: {}", weather.current.condition.icon); 
         }
         Err(e) => {

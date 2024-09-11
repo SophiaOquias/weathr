901d4e099222b590cb0613ae4bcd0682f4ff0b10
Sophia <sophia.oquias@gmail.com>
2024-09-11 14:42:31 +0800

    currently broken

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -19,6 +19,8 @@ pub struct Current {
     pub temp_c: f32, 
     pub temp_f: f32, 
     pub condition: Condition, 
+    pub feelslike_c: f32, 
+    pub feelslike_f: f32,
 }
 
 #[derive(Deserialize)]
@@ -28,10 +30,24 @@ pub struct Condition {
     pub code: i32, 
 }
 
+#[derive(Deserialize)]
+pub struct Coordinates {
+    pub loc: String,
+}
+
+pub fn get_coords() -> Result<Coordinates, Box<dyn std::error::Error>> {
+    let api_key = "d78d6f70c265c9".to_string(); 
+    let url = format!("http://ipinfo.io?token={}", api_key);
+    
+    let resp: Coordinates = reqwest::blocking::get(url)?.json()?;
+    
+    Ok(resp)
+}
+
 pub fn get_weather(query: String) -> Result<WeatherResponse, Box<dyn std::error::Error>> {
     let api_key = "76436078e0454ee8bd9131745240609".to_string();
     let url = format!(
-        "http://api.weatherapi.com/v1/current.json?key={}&q={}",
+        "http://api.weatherapi.com/v1/current.json?key={}&q={}&aqi=no",
             api_key, 
             query
     );
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,23 @@
 #![allow(unused)]
 
 mod api; 
-mod utils; 
-// TODO: edit interface 
 use text_to_ascii_art::to_art; 
-
 // TODO: get input via cli using clap? 
+// TODO: get current location 
+// TODO: accept argument for temp unit f or c, default in c 
 fn main() {
-    let query = "cebu".to_string(); 
-    match api::get_weather(query) {
+    // let query = get_coords()
+    //     .expect("Could not get current location");  
+    match api::get_weather("lahug".to_string()) {
         Ok(weather) => {
-            match to_art(format!("{}*", weather.current.temp_c), "", 0, 0, 0) {
+            match to_art(format!("{}*", weather.current.temp_c), "", 1, 0, 0) {
                 Ok(string) => println!("{}", string),
                 Err(err) => {
                     println!("{}⁰", weather.current.temp_c); 
                 },
             }
+
+            println!("Feels like {}⁰", weather.current.feelslike_c); 
 
             println!("{}, {}, {}", 
                 weather.location.name, 
@@ -23,10 +25,6 @@ fn main() {
                 weather.location.country
             ); 
             println!("{}", weather.current.condition.text); 
-
-            // too slow and image has borders that i can't get rid of 
-            // utils::retrieve_image(weather.current.condition.icon); 
-            // utils::display_image("src/weather_icon.png".to_string()); 
         }
         Err(e) => {
             eprintln!("Error: {}", e); 
